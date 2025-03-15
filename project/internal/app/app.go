448569c05b3ed81458190d7eb43148e274a9a0ca
Vinayak Vispute <vinayakvispute4@gmail.com>
2025-03-15 18:18:15 +0530

    Add database connection and migration setup with user and workout tables

--- project/internal/app/app.go
+++ project/internal/app/app.go
@@ -1,20 +1,36 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/vinayakvispute/project/internal/api"
+	"github.com/vinayakvispute/project/internal/store"
+	"github.com/vinayakvispute/project/migrations"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+
+	if err != nil {
+		panic(err)
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// our store will go here
@@ -25,6 +41,7 @@ func NewApplication() (*Application, error) {
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 	return app, nil
 }
