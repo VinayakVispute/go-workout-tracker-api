38ffe4be782f84ce988e827ece977e00fcb65d34
Vinayak Vispute <vinayakvispute4@gmail.com>
2025-04-07 01:15:55 +0530

    Add utility functions for JSON response and ID parameter handling

---
+++ project/internal/utils/utils.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Envelop map[string]interface{} // interface{} is a generic type that can hold any value
+// Envelop is a map that can hold any type of data. It is used to create a JSON response with a single key-value pair.
+
+func WriteJson(w http.ResponseWriter, status int, data Envelop) error {
+
+	js, err := json.MarshalIndent(data, "", "	")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
+
+func ReadIDParam(r *http.Request) (int64, error) {
+
+	paramsId := chi.URLParam(r, "id")
+
+	if paramsId == "" {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	id, err := strconv.ParseInt(paramsId, 10, 64)
+
+	if err != nil {
+		return 0, errors.New("invalid id parameter type")
+	}
+
+	return id, nil
+}
